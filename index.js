edbc16611b1326b04bae505f9fef995f36b4bdf6
kingz75 <adamsbaki04@gmail.com>
2024-11-21 11:32:21 +0100

    first-commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// Function to add items to cart (stored in localStorage)
+function addToCart(productName, productPrice) {
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+    const product = { name: productName, price: productPrice };
+    cart.push(product);
+    localStorage.setItem('cart', JSON.stringify(cart));
+    alert(`${productName} has been added to your cart!`);
+}
+
+// Function to view items in the cart
+function viewCart() {
+    const cart = JSON.parse(localStorage.getItem('cart')) || [];
+    console.log("Cart:", cart);
+}
+
+
+
